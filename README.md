9208a3c91d13b507944d6b0374a429f30880c81d
tjwllms52 <72267692+tjwllms52@users.noreply.github.com>
2023-04-05 23:21:05 -0400

    Uploaded setup.py, and README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+## Here is an example
+
+```python
+import ChatCAT
+
+while True:
+  prompt = input('prompt: ')
+  ChatCAT.get_response(prompt)
+```
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'chatbot'
+LONG_DESCRIPTION = 'Just a chatbot'
+
+# Setting up
+setup(
+    name="ChatCAT",
+    version=VERSION,
+    author="RGB_CATT",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['typing'],
+    keywords=['python', 'chat', 'bot', 'chatbot', 'chatting robot'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
