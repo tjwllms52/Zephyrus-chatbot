a8867166c269f0dc85c694b7ee5c62da8730aedf
tjwllms52 <72267692+tjwllms52@users.noreply.github.com>
2023-05-08 14:26:01 -0400

    Update main.py

--- CHATBOT/main.py
+++ CHATBOT/main.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_json(prompt):
-    return requests.get('https://zephyrus-chatbot.tjwllms52.repl.co/api/response?prompt=hi')
+    return requests.get('https://zephyrus-chatbot.tjwllms52.repl.co/api/response?prompt=%s' % prompt)
 
 def get_response(prompt):
     return get_json(prompt)['message']['CHATBOT']
