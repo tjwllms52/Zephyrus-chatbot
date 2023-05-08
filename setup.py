b711f7ed1ae0764e2d6f4abfa45683b551884b76
tjwllms52 <72267692+tjwllms52@users.noreply.github.com>
2023-05-08 14:23:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = 'Just a chatbot'
 
 # Setting up
 setup(
-    name="ChatCAT",
+    name="ZephyrusChatbot",
     version=VERSION,
     author="RGB_CATT",
     author_email="",
@@ -21,14 +21,10 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['typing'],
-    keywords=['python', 'chat', 'bot', 'chatbot', 'chatting robot'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+    install_requires=['requests'],
+    keywords=['python', 'chat', 'bot', 'chatbot'],
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+])
